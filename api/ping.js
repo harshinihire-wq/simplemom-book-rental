5d852c9fd5344f5aae18540a6aa1da13a0cbeb3b
harshinihire-wq <harshini.hire@gmail.com>
2025-11-07 16:24:51 +0530

    Delete api directory

--- api/ping.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    hasSecret: !!process.env.NOTION_SECRET,
-    hasDb: !!process.env.NOTION_DATABASE_ID
-  });
-}
